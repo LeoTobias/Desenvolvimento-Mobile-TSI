ad135eb3a85da68817c43c3824b4b6be1b219727
LEONARDO TOBIAS <leonardo.tobias@senacsp.edu.br>
2023-04-04 20:34:59 -0300

    Aula sobre cadastros

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@senac.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Michel',
+    email: 'michel@senac.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'rafael',
+    email: 'rafael@senac.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'jackson',
+    email: 'jackson@senac.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png',
+  ),
+  '5': const User(
+    id: '5',
+    name: 'vinicius',
+    email: 'vinicius@senac.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png',
+  ),
+  '6': const User(
+    id: '6',
+    name: 'leonardo',
+    email: 'leonardo@senac.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2012/04/01/18/02/golf-23794_960_720.png',
+  ),
+};
---
+++ flutter_crud/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/views/user_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
